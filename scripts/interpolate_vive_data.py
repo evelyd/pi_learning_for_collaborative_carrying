c5785239b1bb9bc9425a56c7ada33f37f76a4e27
Evelyn D'Elia <17877131+evelyd@users.noreply.github.com>
2024-10-31 22:13:57 +0100

    fix populating of dict fields in interpolation script

--- scripts/interpolate_vive_data.py
+++ scripts/interpolate_vive_data.py
@@ -8,24 +8,29 @@ def interpolate_data(original_data_dict, target_timestamps):
     """
     Interpolate the original data to match the target timestamps.
     """
+    interpolated_data = {}
 
     # goes throught the elements, not the timesteps, of the actual data (i.e. goes through each of 4 elems of orientation)
     for key in original_data_dict.keys():
-        interpolated_positions = np.zeros((len(target_timestamps), original_data_dict[key]['positions'].shape[1]))
-        interpolated_orientations = np.zeros((len(target_timestamps), original_data_dict[key]['orientations'].shape[1]))
-        interpolated_data = {key: {} for key in original_data_dict.keys()}
+        positions = original_data_dict[key]['positions']
+        orientations = original_data_dict[key]['orientations']
+        timestamps = original_data_dict[key]['timestamps']
+        interpolated_positions = np.zeros((len(target_timestamps), positions.shape[1]))
+        interpolated_orientations = np.zeros((len(target_timestamps), orientations.shape[1]))
 
         for i in range(original_data_dict[key]['positions'].shape[1]):
-            interp_func = interp1d(original_data_dict[key]['timestamps'], original_data_dict[key]['positions'][:, i], kind='linear', fill_value="extrapolate")
+            interp_func = interp1d(timestamps, original_data_dict[key]['positions'][:, i], kind='linear', fill_value="extrapolate")
             interpolated_positions[:, i] = interp_func(target_timestamps)
 
         for i in range(original_data_dict[key]['orientations'].shape[1]):
-            interp_func = interp1d(original_data_dict[key]['timestamps'], original_data_dict[key]['orientations'][:, i], kind='linear', fill_value="extrapolate")
+            interp_func = interp1d(timestamps, original_data_dict[key]['orientations'][:, i], kind='linear', fill_value="extrapolate")
             interpolated_orientations[:, i] = interp_func(target_timestamps)
 
-        interpolated_data[key]['positions'] = np.array(interpolated_positions)
-        interpolated_data[key]['orientations'] = np.array(interpolated_orientations)
-        interpolated_data[key]['timestamps'] = target_timestamps
+        interpolated_data[key] = {
+                'positions': interpolated_positions,
+                'orientations': interpolated_orientations,
+                'timestamps': target_timestamps
+            }
 
     return interpolated_data
 
@@ -39,12 +44,10 @@ def main(data_location):
 
     # Get paths to data
     script_directory = os.path.dirname(os.path.abspath(__file__))
-    # follower_log_file_path = os.path.join(script_directory, data_location + "/follower/data.log")
     vive_data_file = os.path.join(script_directory, data_location + "vive/parsed_vive_data.mat")
-    #TODO take only from one ifeel file?
+    # Taking the timesteps from the leader iFeel data
     ifeel_data_file = os.path.join(script_directory, data_location + "leader/parsed_ifeel_data.mat")
-    # leader_mat_file_path = os.path.join(script_directory, data_location + "/leader/parsed_ifeel_data.mat")
-    output_file = os.path.join(script_directory, data_location + "interpolated_vive_data.mat")
+    output_file = os.path.join(script_directory, data_location + "/vive/interpolated_vive_data.mat")
 
     # Start all the data at the same time so that it lines up
     start_ind_dict = {"forward_backward": [3100, 2049, 48415], "left_right": [10275, 4539, 2411]}
@@ -56,7 +59,6 @@ def main(data_location):
         KeyError("Start indices not defined for this data.")
 
     # Load the original data
-    # original_vive_data = h5py.File(vive_data_file, 'r')
     original_vive_data = scipy.io.loadmat(vive_data_file)
     cut_data = {key: {} for key in original_vive_data.keys() if key not in ['__header__', '__version__', '__globals__']}
     for key in original_vive_data.keys():
@@ -80,15 +82,14 @@ def main(data_location):
     target_timestamps = ifeel_data['timestamps'][0]
     target_timestamps = target_timestamps[start_indices[0]:] - target_timestamps[start_indices[0]]
 
-    print(cut_data['openxr_head2']['positions'])
-
     # Interpolate the data
     interpolated_vive_data = interpolate_data(cut_data, target_timestamps)
 
     # Plot the original and interpolated position data for some frames just to check
+    key_to_plot = 'vive_tracker_left_elbow_pose2'
     plt.figure(1)
-    plt.plot(cut_data['vive_tracker_waist_pose']['timestamps'], cut_data['vive_tracker_waist_pose']['positions'][:, 1], label='Original follower waist z')
-    plt.plot(interpolated_vive_data['vive_tracker_waist_pose']['timestamps'], interpolated_vive_data['vive_tracker_waist_pose']['positions'][:, 1], linestyle="--", label='Interp follower waist z')
+    plt.plot(cut_data[key_to_plot]['timestamps'], cut_data[key_to_plot]['positions'][:, 1], label='Original leader waist z')
+    plt.plot(interpolated_vive_data[key_to_plot]['timestamps'], interpolated_vive_data[key_to_plot]['positions'][:, 1], linestyle="--", label='Interp leader waist z')
     plt.legend()
     plt.show()
 
