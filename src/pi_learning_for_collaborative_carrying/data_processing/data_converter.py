update save, simplify quaternion processing, and correct headset angle

src/pi_learning_for_collaborative_carrying/data_processing/data_converter.py
@@ -159,19 +159,16 @@ class DataConverter:
 
             # Store orientation task data
             elif item_type == "SO3Task":
-                # Assumes wxyz format
-                quaternions = [utils.normalize_quaternion(quat) for quat in mocap_data_cleaned['node' + str(item['node_number'])]['orient']]
-                # Rotate into the vive world frame
-                angular_velocities = mocap_data_cleaned['node' + str(item['node_number'])]['gyro']
+                # Assumes wxyz format for raw data
+                quaternions = [utils.normalize_quaternion(utils.to_xyzw(quat)) for quat in mocap_data_cleaned['node' + str(item['node_number'])]['orient']]
 
-                # Normalize the quaternions
-                quaternions = [utils.normalize_quaternion(quat) for quat in quaternions]
+                angular_velocities = mocap_data_cleaned['node' + str(item['node_number'])]['gyro']
 
                 task = motion_data.SO3Task(name=key, orientations=quaternions, angular_velocities=angular_velocities)
                 motiondata.SO3Tasks.append(asdict(task))
 
                 # Update node struct for calibration
-                I_R_IMU_calib = manif.SO3(quaternion=utils.normalize_quaternion(utils.to_xyzw(np.array(quaternions[0]))))
+                I_R_IMU_calib = manif.SO3(quaternion=np.array(quaternions[0]))
                 I_omega_IMU_calib = manif.SO3Tangent(angular_velocities[0])
 
                 nodeData = baf.ik.nodeData()
@@ -182,14 +179,14 @@ class DataConverter:
             # Store gravity task data
             elif item_type == "GravityTask":
 
-                # Assumes wxyz format
-                quaternions = [utils.normalize_quaternion(quat) for quat in mocap_data_cleaned['node' + str(item['node_number'])]['orient']]
+                # Assumes wxyz format for raw data
+                quaternions = [utils.normalize_quaternion(utils.to_xyzw(quat)) for quat in mocap_data_cleaned['node' + str(item['node_number'])]['orient']]
 
                 task = motion_data.GravityTask(name=key, orientations=quaternions)
                 motiondata.GravityTasks.append(asdict(task))
 
                 # Update node struct for calibration
-                I_R_IMU_calib = manif.SO3(quaternion=utils.normalize_quaternion(utils.to_xyzw(np.array(quaternions[0]))))
+                I_R_IMU_calib = manif.SO3(quaternion=np.array(quaternions[0]))
                 I_omega_IMU_calib = manif.SO3Tangent(mocap_data_cleaned['node' + str(item['node_number'])]['gyro'][0])
 
                 nodeData = baf.ik.nodeData()

src/pi_learning_for_collaborative_carrying/data_processing/ifeel_data_retargeter.py
@@ -227,7 +227,7 @@ class WBGR:
 
                 elif task_type == 'SO3Task':
                     # Get the orientation data in manif SO3 format, assuming it comes from wxyz form
-                    I_R_IMU_manif = manif.SO3(quaternion=utils.to_xyzw(I_quat_IMU))
+                    I_R_IMU_manif = manif.SO3(quaternion=I_quat_IMU)
 
                     # Get the angular velocity data in manif SO3Tangent format
                     I_omega_IMU = np.array(task['angular_velocities'][i])
@@ -237,7 +237,7 @@ class WBGR:
 
                 else: # for GravityTask
                     # Get the orientation data in manif SO3 format, assuming it comes from wxyz form
-                    I_R_IMU_manif = manif.SO3(quaternion=utils.to_xyzw(I_quat_IMU))
+                    I_R_IMU_manif = manif.SO3(quaternion=I_quat_IMU)
 
                     assert self.humanIK.updateGravityTask(node_number, I_R_IMU_manif)
 